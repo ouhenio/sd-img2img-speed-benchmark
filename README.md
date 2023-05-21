e662c090ac01e1ff4a2689041200839419e4d3d5
ouhenio <eiherrera@uc.cl>
2023-05-21 15:05:28 -0400

    update benchmarks

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Stable Diffusion img2img speed benchmarks
 
 The goal of this repo is to store methods and metrics related to the inference time of Stable Diffusion's img2img.
+
+|                          Method | speed (seconds) |
+|--------------------------------:|----------------:|
+|                         Vanilla |            8.72 |
+|                      `float.16` |            3.28 |
+|                 `torch.compile` |            8.58 |
+|     `torch.compile`+ `float.16` |            3.15 |
+
+The measurements in this tables where made with a 3090.
--- main.py
+++ main.py
@@ -1,34 +1,50 @@
-import time
+import torch
 
 from diffusers import StableDiffusionImg2ImgPipeline as pipeline
+from torch.utils.benchmark import Timer
 from PIL import Image
 
-# Time measurement decorator
-def measure_inferece_speed(inference_method):
-    def wrapper(pipe, prompt, image):
-        start_time = time.time()
-        inference_method(pipe, prompt, image, strength=0.75)
-        end_time = time.time()
-        inference_time = end_time - start_time
-
-        print("Inference Time:", inference_time)
-
-    return wrapper
-
-# Inference wrapper
-@measure_inferece_speed
-def infer(pipe, prompt, image, strength):
-    pipe(prompt=prompt, image=image, strength=strength)
-
-# Sample image
-init_img = Image.open("image.jpg").convert("RGB")
-init_img = init_img.resize((768, 512))
-
-# Basic img2img pipeline
-pipe = pipeline.from_pretrained("runwayml/stable-diffusion-v1-5")
-pipe.to("cuda")
-
-# Sample prompt
+device = "cuda"
+model = "runwayml/stable-diffusion-v1-5"
+image_path = "image.jpg"
 prompt = "a picture of the andes mountains in winter"
 
-infer(pipe=pipe, prompt=prompt, image=init_img)
+# Sample image
+init_img = Image.open(image_path).convert("RGB")
+init_img = init_img.resize((768, 512))
+
+# Speed benchmarking wrapper
+def benchmark_torch_function(f, *args, **kwargs):
+    t0 = Timer(
+        stmt="f(*args, **kwargs)", globals={"args": args, "kwargs": kwargs, "f": f}
+    )
+    return round(t0.blocked_autorange(min_run_time=1).mean, 2)
+
+# Basic img2img pipeline
+pipe = pipeline.from_pretrained(model).to(device)
+f = lambda : pipe(prompt=prompt, image=init_img)
+time_basic_pipeline = benchmark_torch_function(f)
+
+# Half-precision pipeline
+pipe = pipeline.from_pretrained(model,torch_dtype=torch.float16).to(device)
+f = lambda : pipe(prompt=prompt, image=init_img)
+time_hp_pipeline = benchmark_torch_function(f)
+
+# Compiled pipeline
+pipe = pipeline.from_pretrained(model).to(device)
+pipe.unet = torch.compile(pipe.unet)
+pipe(prompt=prompt, image=init_img) # compile warmup
+f = lambda : pipe(prompt=prompt, image=init_img)
+time_compiled_pipeline = benchmark_torch_function(f)
+
+# Compiled half-precision pipeline
+pipe = pipeline.from_pretrained(model,torch_dtype=torch.float16).to(device)
+pipe.unet = torch.compile(pipe.unet)
+pipe(prompt=prompt, image=init_img) # compile warmup
+f = lambda : pipe(prompt=prompt, image=init_img)
+time_compiled_hp_pipeline = benchmark_torch_function(f)
+
+print(f"Basic pipeline inference time: {time_basic_pipeline}s")
+print(f"Half precision pipeline inference time: {time_hp_pipeline}s")
+print(f"Compiled pipeline inference time: {time_compiled_pipeline}s")
+print(f"Compiled hp pipeline inference time: {time_compiled_hp_pipeline}s")
